b0e9e41188d53094e5bfa9c2997625164af91342
selaaulia <selaaulia1307@gmail.com>
2021-12-12 23:17:17 +0700

    Update

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:4000",
+ baseURL:"http://152.67.108.132:4000",
 });
 
 export default api;
